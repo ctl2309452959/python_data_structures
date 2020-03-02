0fa734974dd1d7cda2bc37036f8d50f85384a113
Smart <2309452959@qq.com>
2020-03-02 10:07:08 +0800

    算法分析-大O符号

---
+++ DS1-ieetcode/practise02.py
@@ -0,0 +1,33 @@
+"""
+T(n)(实际值) =   3*n^2 + 2n +4
+只取变化最快的
+O(n^2)(近似值)
+
+常量=1
+赋值=n
+
+"""
+# a = 5     1
+# b = 6     1
+# c = 10    1
+
+# for i in range(n):        
+#     for j in range(n):    3*n^2
+#         x = i*i
+#         y = z*z
+#         z = i*j
+# for k in range(n):     2n
+#     w = a*k + 100 
+#     v = b*b
+
+# d = 300    1
+
+
+
+
+
+
+
+
+
+
